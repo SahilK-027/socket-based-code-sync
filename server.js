5ed700032c6134c08e1ece07fb41330eb41fa7d3
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-05-08 20:37:11 +0530

    🚀 Deploy...

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const userSocketMap = {};
+// Handling deployment
+app.use(express.static('build'));
+app.use((req, res, next)=>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
+
+
+const userSocketMap = {};
 function getAllConnectedClients(roomId) {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
         return {
